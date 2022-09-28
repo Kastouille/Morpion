e08a638523986c7ea63802a472b353c50e259009
Kastouille <110926139+Kastouille@users.noreply.github.com>
2022-09-28 08:40:53 +0200

    Update morpion.py
    Mise à jour du code, amélioration de la documentation et ajout de la possibilité de match nul.

--- morpion.py
+++ morpion.py
@@ -1,3 +1,12 @@
+#   __  __                  _             
+#  |  \/  |                (_)            
+#  | \  / | ___  _ __ _ __  _  ___  _ __  
+#  | |\/| |/ _ \| '__| '_ \| |/ _ \| '_ \ 
+#  | |  | | (_) | |  | |_) | | (_) | | | |
+#  |_|  |_|\___/|_|  | .__/|_|\___/|_| |_|
+#                    | |                  
+#                    |_|     
+
 from random import randint
 from numpy import matrix
 from time import sleep
@@ -7,31 +16,45 @@ grille = [[" ", " ", " "],
           [" ", " ", " "]]
 
 def morpion():
+    """
+    Le jeu du morpion dans la console Python, avec un joueur AI.
+    Amusez-vous à jouer au morpion contre l'ordinateur !
+    (Relancez le programme à chaque fin de partie pour réinitialiser les variables)
+    Syntaxe :
+        morpion() --> Le jeu commence
+    """
     gagnant = False
+    mouvements = 0
     while gagnant == False:
-        #Coordonnées:
+        #Coordonnées du joueur:
         ligne = int(input("Veuillez rentrer le numéro de la ligne (1-3)")) - 1
         case = int(input("Veuillez renterer le numéro de la case (1-3)")) - 1
         vider()
         #Placement des pions
         if libre(ligne, case) == True: #Vous
             grille[ligne][case] = "X"
+            mouvements += 1
             print(matrix(grille))
             sleep(1)
             vider()
         elif libre(ligne, case) == False:
-            print("Cette case est déjà prise !")
+            print("Cette case est déjà prise, veuillez reccomencer !")
+            continue
         ai_player() #Ordinateur
+        mouvements += 1
         print(matrix(grille))
         sleep(1)
-        gagnant = gagne()
-    return "Le gagnant est", gagnant
+        gagnant = gagne(mouvements)
+    vider()
+    return "Gagnant :", gagnant
 
 
 def libre(l, c):
+    #Vérifie si la case est libre
     return grille[l][c] == " "
 
 def ai_player():
+    #Joueur automatique
     ligne = randint(0, 2)
     case = randint(0, 2)
     if libre(ligne, case) == True:
@@ -39,32 +62,41 @@ def ai_player():
     elif libre(ligne, case) == False:
         ai_player()
 
-def gagne():
+def gagne(m):
+    #Vérifie si les conditions de victoires sont réunies
+    #et pour quel joueur, retourne le gagnant
+    
     #Lignes horizontales:
     for ligne in grille:
         if ligne == ["X", "X", "X"]:
-            return "X"
+            return "Vous"
         elif ligne == ["O", "O", "O"]:
-            return "O"
+            return "Ordinateur"
 
     #Lignes verticales:
     ligne_vert = [[ligne[0] for ligne in grille], [ligne[1] for ligne in grille], [ligne[2] for ligne in grille]]
     for ligne in ligne_vert:
         if ligne == ["X", "X", "X"]:
-            return "X"
+            return "Vous"
         elif ligne == ["O", "O", "O"]:
-            return "O"
+            return "Ordinateur"
 
     #Diagonales:
     diago = [[grille[0][0], grille[1][1], grille[2][2]], [grille[0][2], grille[1][1], grille[2][0]]]
     for ligne in diago:
         if ligne == ["X", "X", "X"]:
-            return "X"
+            return "Vous"
         elif ligne == ["O", "O", "O"]:
-            return "O"
-
+            return "Ordinateur"
+    
+    #Match nul:
+    if m == 9:
+        return "Aucun, match nul !"
+    
+    #Aucune condition remplie
     return False
 
 def vider():
+    #Vide l'écran entre chaque mouvements
     for i in range(50):
         print(" ")
